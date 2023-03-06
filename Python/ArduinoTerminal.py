04af376f78730e2bad6c33feb7d650ec015f02cf
Alecsander Lima <93883349+aleclima14@users.noreply.github.com>
2023-03-06 01:04:28 -0300

    Organizing code and implementing serial reading

--- Python/ArduinoTerminal.py
+++ Python/ArduinoTerminal.py
@@ -1,7 +1,7 @@
 import sys
 
-from PyQt6.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QLineEdit, 
-QComboBox, QVBoxLayout, QHBoxLayout, QGridLayout)
+from PyQt6.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, 
+QComboBox, QVBoxLayout, QHBoxLayout)
 
 from PyQt6.QtSerialPort import (QSerialPort, QSerialPortInfo)
 from PyQt6 import QtCore
@@ -13,69 +13,67 @@ class Windows(QWidget):
       self.initUI()
       
    def initUI(self):
-      self.setWindowIcon(QIcon("Python/led-icon.png"))
+      self.setWindowIcon(QIcon("led-icon.png"))
       self.setWindowTitle("Serial LED Control")
       self.setContentsMargins(20, 20, 20, 20)
-      # self.resize(400, 170)
-      self.setFixedSize(420,150)
+      self.setFixedSize(420, 180)
+
+      self.serialReadLabel = QLabel()
+      self.availablePorts = QComboBox()
+      self.baudrateComPort = QComboBox()
+      self.serialPort = QSerialPort()
+      self.refreshButton = QPushButton("R")
+      self.connectButton = QPushButton("Connect")
+      self.disconnectButton = QPushButton("Disconnect")
+      self.onLedButton = QPushButton("LED ON")
+      self.offLedButton = QPushButton("LED OFF")
 
       layout = QVBoxLayout()
-      self.setLayout(layout)
-
       horizontalBox = QHBoxLayout()
+      verticalBox = QVBoxLayout()
+
+      self.setLayout(layout)    
       horizontalBox.setSpacing(5)
       layout.addLayout(horizontalBox)
+      verticalBox.setSpacing(5)
+      layout.addLayout(verticalBox)
       
-      self.availablePorts = QComboBox()
       self.availablePorts.addItems([comPorts.portName() for comPorts in QSerialPortInfo().availablePorts()])
       self.availablePorts.setMaximumWidth(80)
       self.availablePorts.setMinimumWidth(80)
       
-      self.baudrateComPort = QComboBox()
       self.baudrateComPort.addItems(["9600", "19200", "115200"])
       self.baudrateComPort.setMaximumWidth(80)
-      self.baudrateComPort.setMinimumWidth(80)
-      
-      self.serialPort = QSerialPort()
+      self.baudrateComPort.setMinimumWidth(80)  
 
-      self.refreshButton = QPushButton("R")
       self.refreshButton.setMaximumWidth(25)
       self.refreshButton.setMinimumWidth(25)
       self.refreshButton.clicked.connect(self.refreshComPortEvent)
       
-      self.connectButton = QPushButton("Connect")
       self.connectButton.setMaximumWidth(80)
       self.connectButton.setMinimumWidth(80)
       self.connectButton.clicked.connect(self.connectButtonEvent)
       
-      self.desconnectButton = QPushButton("Desconnect")
-      self.desconnectButton.setMaximumWidth(80)
-      self.desconnectButton.setMinimumWidth(80)
-      self.desconnectButton.clicked.connect(self.desconnectButtonEvent)
-      self.desconnectButton.setEnabled(False)
+      self.disconnectButton.setMaximumWidth(80)
+      self.disconnectButton.setMinimumWidth(80)
+      self.disconnectButton.clicked.connect(self.disconnectButtonEvent)
+      self.disconnectButton.setEnabled(False)
+      
+      self.onLedButton.clicked.connect(self.onButtonEvent)
+      self.onLedButton.setEnabled(False)
+
+      self.offLedButton.clicked.connect(self.offButtonEvent)
+      self.offLedButton.setEnabled(False)
 
       horizontalBox.addWidget(self.availablePorts)
       horizontalBox.addWidget(self.refreshButton)
       horizontalBox.addWidget(self.baudrateComPort)
       horizontalBox.addWidget(self.connectButton)
-      horizontalBox.addWidget(self.desconnectButton)
-
-      verticalBox = QVBoxLayout()
-      verticalBox.setSpacing(5)
-      layout.addLayout(verticalBox)
-
-      self.onLedButton = QPushButton("LED ON")
-      self.onLedButton.clicked.connect(self.onButtonEvent)
-      self.onLedButton.setEnabled(False)
-
-      self.offLedButton = QPushButton("LED OFF")
-      self.offLedButton.clicked.connect(self.offButtonEvent)
-      self.offLedButton.setEnabled(False)
-
-      self.serialReadLabel = QLabel()
+      horizontalBox.addWidget(self.disconnectButton)
 
       verticalBox.addWidget(self.onLedButton)
       verticalBox.addWidget(self.offLedButton)
+      verticalBox.addWidget(self.serialReadLabel)
       
    def refreshComPortEvent(self):
       self.availablePorts.clear()
@@ -91,26 +89,32 @@ class Windows(QWidget):
       self.refreshButton.setEnabled(False)
       self.availablePorts.setEnabled(False)
       self.baudrateComPort.setEnabled(False)
-      self.desconnectButton.setEnabled(True)
+      self.disconnectButton.setEnabled(True)
       self.onLedButton.setEnabled(True)
       self.offLedButton.setEnabled(True)
 
-   def desconnectButtonEvent(self):
+   def disconnectButtonEvent(self):
       self.serialPort.close()
 
       self.connectButton.setEnabled(True)
       self.refreshButton.setEnabled(True)
       self.availablePorts.setEnabled(True)
       self.baudrateComPort.setEnabled(True)
-      self.desconnectButton.setEnabled(False)
+      self.disconnectButton.setEnabled(False)
       self.onLedButton.setEnabled(False)
       self.offLedButton.setEnabled(False)
 
    def onButtonEvent(self):
       self.serialPort.write("on".encode())
+      self.readStatusLed()
    
    def offButtonEvent(self):
       self.serialPort.write("off".encode())
+      self.readStatusLed()
+     
+   def readStatusLed(self):
+      valorLido = str(self.serialPort.readAll())
+      self.serialReadLabel.setText(valorLido)
 
 def window():
    applicationWindow = QApplication(sys.argv)
